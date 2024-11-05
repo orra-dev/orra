2b004d5dde2cf43fc7c974084e4cb9d6615660c3
Ezo Saleh <105397+ezodude@users.noreply.github.com>
2024-11-05 17:33:44 +0000

    Use narwhal for CLI version.

--- cli/cmd/version.go
+++ cli/cmd/version.go
@@ -6,12 +6,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func newVersionCmd(opts *CliOpts) *cobra.Command {
+func newVersionCmd(_ *CliOpts) *cobra.Command {
 	return &cobra.Command{
 		Use:   "version",
 		Short: "Print the client and server version information",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			fmt.Println("Client Version: v0.1.0")
+			fmt.Println("Client Version: v0.1.0-narwhal")
 			// TODO: Implement server version check
 			return nil
 		},
