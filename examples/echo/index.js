12aaee77dfbf84339e28535160ec5569db49e10e
Ezo Saleh <105397+ezodude@users.noreply.github.com>
2024-11-12 10:26:26 +0000

    Use @orra.dev as org for JS SDK in echo example.

--- examples/echo/index.js
+++ examples/echo/index.js
@@ -4,7 +4,7 @@
  *  file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-import { createClient } from '@orra/sdk';
+import { createClient } from '@orra.dev/sdk';
 import dotenv from 'dotenv';
 
 // Load environment variables
